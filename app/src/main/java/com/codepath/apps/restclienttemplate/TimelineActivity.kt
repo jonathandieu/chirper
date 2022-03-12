ffed2383366947b0b03a4cbd3d56fb22ed7ba5e9
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-03-12 17:24:35 -0500

    wip

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
@@ -2,10 +2,35 @@ package com.codepath.apps.restclienttemplate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
 
 class TimelineActivity : AppCompatActivity() {
+
+    lateinit var client: TwitterClient // Declare client
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timeline)
+
+        client = TwitterApplication.getRestClient(this)
+    }
+
+    fun populateHomeTimeline() {
+        client.getHomeTimeline(object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                TODO("Not yet implemented")
+            }
+
+
+        })
     }
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.kt
@@ -52,16 +52,17 @@ class TwitterClient(context: Context) : OAuthBaseClient(
             "intent://%s#Intent;action=android.intent.action.VIEW;scheme=%s;package=%s;S.browser_fallback_url=%s;end"
     }
 
-    // CHANGE THIS
+
     // DEFINE METHODS for different API endpoints here
     // We want to pull down a whole timeline for a user
-    fun getInterestingnessList(handler: JsonHttpResponseHandler) {
+    fun getHomeTimeline(handler: JsonHttpResponseHandler) {
         val apiUrl =
-            getApiUrl("?nojsoncallback=1&method=flickr.interestingness.getList")
+            getApiUrl("statuses/home_timeline.json")
 
         // Can specify query string params directly or through RequestParams.
         val params = RequestParams()
-        params.put("format", "json")
+        params.put("count", "25")
+//        params.put("since_id", 1)
         client.get(apiUrl, params, handler)
     }
 
